7da4259e8acbbcf6e2bd9d27ed434ae94f79a193
Ismail KHAIRY <tawazlwb@gmail.com>
2022-09-30 21:35:49 +0200

    49. useEffect - Dependency List

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import App from './App'
 
 const container = document.getElementById('root')
 const root = createRoot(container) // createRoot(container!) if you use TypeScript
-root.render(<App tab='home' />)
+root.render(
+  <React.StrictMode>
+    <App tab='home' />
+  </React.StrictMode>
+)
