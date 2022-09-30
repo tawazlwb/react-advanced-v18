2dd034598cd942b8009c821d3a5f344b98c897ab
Ismail KHAIRY <tawazlwb@gmail.com>
2022-09-30 17:23:10 +0200

    41. useState - Array Example

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from 'react'
+import { data } from '../../../data'
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+  // State
+  const [people, setPeople] = useState(data)
 
-export default UseStateArray;
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+
+  return (
+    <>
+      <h2>useState array example</h2>
+      {people.map((person) => {
+        const { id, name } = person
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        )
+      })}
+      <button type='button' className='btn' onClick={() => setPeople([])}>
+        clear item
+      </button>
+    </>
+  )
+}
+
+export default UseStateArray
