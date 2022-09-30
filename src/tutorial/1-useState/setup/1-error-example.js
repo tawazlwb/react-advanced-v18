372a6dc59e943ab455c788dc06370e7eb70e8d8d
Khairy Ismail <ikheiry@sqli.com>
2022-09-30 16:44:59 +0200

    38. useState - Simple Use Case

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from 'react'
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
-};
+  let title = 'random people'
+  const handleClick = () => {
+    title = 'hello people'
+    console.log(title)
+  }
 
-export default ErrorExample;
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  )
+}
+
+export default ErrorExample
