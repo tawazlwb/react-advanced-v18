2edd13860918bfeabde32d1a16698d998600e2e8
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-04 14:15:20 +0200

    65. useReducer - useState Setup

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,59 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
+
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
-};
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const [showModal, setShowModal] = useState(false)
 
-export default Index;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (name) {
+      setShowModal(true)
+
+      setPeople((oldPeople) => {
+        return [
+          ...oldPeople,
+          {
+            id: new Date().getTime().toString(),
+            name,
+          },
+        ]
+      })
+
+      setName('')
+    } else {
+      setShowModal(true)
+    }
+    console.log('submit')
+  }
+
+  return (
+    <>
+      {showModal && <Modal />}
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type='submit'>submit</button>
+      </form>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Index
