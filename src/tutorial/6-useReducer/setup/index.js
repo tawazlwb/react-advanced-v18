611ec7e9d62aefccbd8213095fe02b5314049e53
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-04 14:25:22 +0200

    66. useReducer - Refactor

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,38 +3,47 @@ import Modal from './Modal'
 import { data } from '../../../data'
 
 // reducer function
+const reducer = (state, action) => {
+  // switch (action.type) {
+  // }
+}
+
+const defaultState = {
+  people: data,
+  isModalOpen: true,
+  modalContent: 'hello world',
+}
 
 const Index = () => {
   const [name, setName] = useState('')
-  const [people, setPeople] = useState(data)
-  const [showModal, setShowModal] = useState(false)
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     if (name) {
-      setShowModal(true)
+      // setShowModal(true)
 
-      setPeople((oldPeople) => {
-        return [
-          ...oldPeople,
-          {
-            id: new Date().getTime().toString(),
-            name,
-          },
-        ]
-      })
+      // setPeople((oldPeople) => {
+      //   return [
+      //     ...oldPeople,
+      //     {
+      //       id: new Date().getTime().toString(),
+      //       name,
+      //     },
+      //   ]
+      // })
 
       setName('')
     } else {
-      setShowModal(true)
+      // setShowModal(true)
     }
     console.log('submit')
   }
 
   return (
     <>
-      {showModal && <Modal />}
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
       <form className='form' onSubmit={handleSubmit}>
         <div>
           <input
@@ -45,7 +54,7 @@ const Index = () => {
         </div>
         <button type='submit'>submit</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>
