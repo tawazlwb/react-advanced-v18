9b3dc7639fdb3266a842585f6be81e0d284e2141
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-05 13:39:11 +0200

    79. useCallback

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -10,6 +10,11 @@ const url = 'https://course-api.com/javascript-store-products'
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
+  const [cart, setCart] = useState(0)
+
+  const addTocart = useCallback(() => {
+    setCart((prevCart) => prevCart + 1)
+  }, [cart])
 
   return (
     <>
@@ -17,12 +22,13 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: '3rem' }}> cart : {cart}</h1>
+      <BigList products={products} addTocart={addTocart} />
     </>
   )
 }
 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addTocart }) => {
   useEffect(() => {
     console.count('big list called')
   })
@@ -30,13 +36,19 @@ const BigList = React.memo(({ products }) => {
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addTocart={addTocart}
+          ></SingleProduct>
+        )
       })}
     </section>
   )
 })
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addTocart }) => {
   useEffect(() => {
     console.count('single item called')
   })
@@ -50,6 +62,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addTocart}>add to cart</button>
     </article>
   )
 }
