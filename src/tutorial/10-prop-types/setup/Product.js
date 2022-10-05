9a94fd01574260abaeaccdf37c98b23a9052da78
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-05 12:34:10 +0200

    75. PropTypes - Default Values

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -2,28 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import defaultImage from '../../../assets/default-image.jpeg'
 
-const Product = ({ id, name, image, price }) => {
+const Product = ({ name, image, price }) => {
+  const url = image && image.url
+
   return (
     <article className='product'>
-      <img src={image.url} alt={name} />
+      <img src={url || defaultImage} alt={name || 'default name'} />
       <h4>{name}</h4>
-      <p>${price}</p>
+      <p>${price || 3.99}</p>
     </article>
   )
 }
 
 Product.propTypes = {
-  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   image: PropTypes.object.isRequired,
 }
 
-Product.defaultProps = {
-  id: 'default name',
-  name: 'default name',
-  price: 3.99,
-  image: defaultImage,
-}
+// Product.defaultProps = {
+//   id: 'default name',
+//   name: 'default name',
+//   price: 3.99,
+//   image: defaultImage,
+// }
 
 export default Product
